5d47219db238f8d4bbe4e5f53cd0f2be0b823d5f
Pekka Enberg <penberg@localhost.(none)>
2005-10-09 18:34:26 +0300

    Replace huge switch statement with lookup table.

--- src/jit/statement.c
+++ src/jit/statement.c
@@ -9,8 +9,75 @@
 #include <stdlib.h>
 #include <assert.h>
 
-/* Convert LDC_<x> operations.  */
-static void convert_ldc_x(struct constant_pool *cp, unsigned long cp_idx,
+static void convert_nop(struct classblock *cb, unsigned char *code, size_t len,
+			struct statement *stmt, struct operand_stack *stack)
+{
+	stmt->type = STMT_NOP;
+}
+
+static void convert_aconst_null(struct classblock *cb, unsigned char *code,
+				size_t len, struct statement *stmt,
+				struct operand_stack *stack)
+{
+	stmt->type = STMT_ASSIGN;
+	stmt->operand.type = CONST_NULL;
+}
+
+static void convert_iconst(struct classblock *cb, unsigned char *code, size_t len,
+			   struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 0);
+	stmt->type = STMT_ASSIGN;
+	stmt->operand.type = CONST_INT;
+	stmt->operand.value = code[0]-OPC_ICONST_0;
+}
+
+static void convert_lconst(struct classblock *cb, unsigned char *code, size_t len,
+			   struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 0);
+	stmt->type = STMT_ASSIGN;
+	stmt->operand.type = CONST_LONG;
+	stmt->operand.value = code[0]-OPC_LCONST_0;
+}
+
+static void convert_fconst(struct classblock *cb, unsigned char *code, size_t len,
+			   struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 0);
+	stmt->type = STMT_ASSIGN;
+	stmt->operand.type = CONST_FLOAT;
+	stmt->operand.fvalue = code[0]-OPC_FCONST_0;
+}
+
+static void convert_dconst(struct classblock *cb, unsigned char *code, size_t len,
+			   struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 0);
+	stmt->type = STMT_ASSIGN;
+	stmt->operand.type = CONST_DOUBLE;
+	stmt->operand.fvalue = code[0]-OPC_DCONST_0;
+}
+
+static void convert_bipush(struct classblock *cb, unsigned char *code, size_t len,
+			   struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 1);
+	stmt->type = STMT_ASSIGN;
+	stmt->operand.type = CONST_INT;
+	stmt->operand.value = (char)code[1];
+}
+
+static void convert_sipush(struct classblock *cb, unsigned char *code, size_t len,
+			   struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 2);
+	stmt->type = STMT_ASSIGN;
+	stmt->operand.type = CONST_INT;
+	stmt->operand.value = (short) be16_to_cpu(*(u2*) &code[1]);
+}
+
+static void __convert_ldc(struct constant_pool *cp, unsigned long cp_idx,
 			  struct statement *stmt)
 {
 	stmt->type = STMT_ASSIGN;
@@ -42,80 +109,64 @@ static void convert_ldc_x(struct constant_pool *cp, unsigned long cp_idx,
 	}
 }
 
+static void convert_ldc(struct classblock *cb, unsigned char *code, size_t len,
+			struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 1);
+	__convert_ldc(&cb->constant_pool, code[1], stmt);
+	stack_push(stack, stmt->target);
+}
+
+static void convert_ldc_w(struct classblock *cb, unsigned char *code, size_t len,
+			  struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 2);
+	__convert_ldc(&cb->constant_pool, be16_to_cpu(*(u2*) &code[1]), stmt);
+	stack_push(stack, stmt->target);
+}
+
+static void convert_ldc2_w(struct classblock *cb, unsigned char *code, size_t len,
+			   struct statement *stmt, struct operand_stack *stack)
+{
+	assert(len > 2);
+	__convert_ldc(&cb->constant_pool, be16_to_cpu(*(u2*) &code[1]), stmt);
+	stack_push(stack, stmt->target);
+}
+
+typedef void (*convert_fn_t)(struct classblock *, unsigned char *, size_t,
+			     struct statement *, struct operand_stack *stack);
+
+static convert_fn_t converters[] = {
+	[OPC_NOP]		= convert_nop,
+	[OPC_ACONST_NULL]	= convert_aconst_null,
+	[OPC_ICONST_M1]		= convert_iconst,
+	[OPC_ICONST_0]		= convert_iconst,
+	[OPC_ICONST_1]		= convert_iconst,
+	[OPC_ICONST_2]		= convert_iconst,
+	[OPC_ICONST_3]		= convert_iconst,
+	[OPC_ICONST_4]		= convert_iconst,
+	[OPC_ICONST_5]		= convert_iconst,
+	[OPC_LCONST_0]		= convert_lconst,
+	[OPC_LCONST_1]		= convert_lconst,
+	[OPC_FCONST_0]		= convert_fconst,
+	[OPC_FCONST_1]		= convert_fconst,
+	[OPC_FCONST_2]		= convert_fconst,
+	[OPC_DCONST_0]		= convert_dconst,
+	[OPC_DCONST_1]		= convert_dconst,
+	[OPC_BIPUSH]		= convert_bipush,
+	[OPC_SIPUSH]		= convert_sipush,
+	[OPC_LDC]		= convert_ldc,
+	[OPC_LDC_W]		= convert_ldc_w,
+	[OPC_LDC2_W]		= convert_ldc2_w,
+};
+
 static void convert_to_stmt(struct classblock *cb, unsigned char *code,
 			    size_t count, struct statement *stmt,
 			    struct operand_stack *stack)
 {
 	assert(count > 0);
-	unsigned char opc = code[0];
-	switch (opc) {
-		case OPC_NOP:
-			stmt->type = STMT_NOP;
-			break;
-		case OPC_ACONST_NULL:
-			stmt->type = STMT_ASSIGN;
-			stmt->operand.type = CONST_NULL;
-			break;
-		case OPC_ICONST_M1:
-		case OPC_ICONST_0:
-		case OPC_ICONST_1:
-		case OPC_ICONST_2:
-		case OPC_ICONST_3:
-		case OPC_ICONST_4:
-		case OPC_ICONST_5:
-			stmt->type = STMT_ASSIGN;
-			stmt->operand.type = CONST_INT;
-			stmt->operand.value = opc-OPC_ICONST_0;
-			break;
-		case OPC_LCONST_0:
-		case OPC_LCONST_1:
-			stmt->type = STMT_ASSIGN;
-			stmt->operand.type = CONST_LONG;
-			stmt->operand.value = opc-OPC_LCONST_0;
-			break;
-		case OPC_FCONST_0:
-		case OPC_FCONST_1:
-		case OPC_FCONST_2:
-			stmt->type = STMT_ASSIGN;
-			stmt->operand.type = CONST_FLOAT;
-			stmt->operand.fvalue = opc-OPC_FCONST_0;
-			break;
-		case OPC_DCONST_0:
-		case OPC_DCONST_1:
-			stmt->type = STMT_ASSIGN;
-			stmt->operand.type = CONST_DOUBLE;
-			stmt->operand.fvalue = opc-OPC_DCONST_0;
-			break;
-		case OPC_BIPUSH:
-			assert(count > 1);
-			stmt->type = STMT_ASSIGN;
-			stmt->operand.type = CONST_INT;
-			stmt->operand.value = (char)code[1];
-			break;
-		case OPC_SIPUSH:
-			assert(count > 2);
-			stmt->type = STMT_ASSIGN;
-			stmt->operand.type = CONST_INT;
-			stmt->operand.value = (short) be16_to_cpu(*(u2*) &code[1]);
-			break;
-		case OPC_LDC:
-			assert(count > 1);
-			convert_ldc_x(&cb->constant_pool, code[1], stmt);
-			stack_push(stack, stmt->target);
-			break;
-		case OPC_LDC_W:
-			assert(count > 2);
-			convert_ldc_x(&cb->constant_pool, be16_to_cpu(*(u2*) &code[1]), stmt);
-			stack_push(stack, stmt->target);
-			break;
-		case OPC_LDC2_W:
-			assert(count > 2);
-			convert_ldc_x(&cb->constant_pool, be16_to_cpu(*(u2*) &code[1]), stmt);
-			stack_push(stack, stmt->target);
-			break;
-		default:
-			assert(!"unknown opcode");
-	};
+	convert_fn_t convert = converters[code[0]];
+	convert(cb, code, count, stmt, stack);
 }
 
 struct statement *stmt_from_bytecode(struct classblock *cb,
