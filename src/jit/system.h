2dc7333f275ddd17bb75b4242ef28aa93ae44686
Pekka Enberg <penberg@haji.ri.fi>
2006-02-20 09:59:46 +0200

    Add missing include file.
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

---
+++ src/jit/system.h
@@ -0,0 +1,10 @@
+#ifndef __JIT_SYSTEM_H
+#define __JIT_SYSTEM_H
+
+#define BITS_PER_LONG (sizeof(unsigned long) * 8)
+
+/* Macros stolen shamelessly from Linux kernel. */
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#define ALIGN(x,a) (((x)+(a)-1)&~((a)-1))
+
+#endif
