32c487eea52d34d7534df8a1066d66c318a0ef23
Jim Huang <jserv@kaffe.org>
2006-03-22 20:19:06 +0200

    [PATCH] Proper acceptance-test script
    Tweak acceptance test script to make sure that we have built jamvm
    classes, and use the same GNU Classpath installation path of jato make
    rules.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- acceptance/run-acceptance-tests.sh
+++ acceptance/run-acceptance-tests.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+# make sure that we have built jamvm classes
+if [ ! -f ../lib/classes.zip ]; then
+  make -C ../lib
+fi
+
+GNU_CLASSPATH_ROOT=`grep "with_classpath_install_dir =" ../jato/Makefile  | grep -v "#" |  awk '{ print $3 }'`
+if test x"$GNU_CLASSPATH_ROOT" = x -o ! -d $GNU_CLASSPATH_ROOT; then
+  echo "Error! Cannot find GNU Classpath installed."
+  exit
+fi
+
 find jamvm/ -name "*.java" | xargs javac
 
-GLIBJ=/usr/local/classpath/share/classpath/glibj.zip
+GLIBJ=$GNU_CLASSPATH_ROOT/share/classpath/glibj.zip
 
 ../jato/jato -Xbootclasspath:../lib/classes.zip:$GLIBJ -cp . jamvm.IntegerArithmeticTest
 if [ $? != 0 ]; then
