a320584cd038895af9c32462e1896596ee4fd6a4
Pekka Enberg <penberg@cs.helsinki.fi>
2006-04-25 13:58:22 +0300

    [PATCH] Use DECLARE_CONVERTER in <jit/arithmetic-bc.h>
    Don't open-code the declaration because that's harder to change.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/jit/arithmetic-bc.h
+++ include/jit/arithmetic-bc.h
@@ -1,45 +1,40 @@
-#ifndef __BYTECODE_ARITHMETIC_CONVERTERS_H
-#define __BYTECODE_ARITHMETIC_CONVERTERS_H
-
-extern int convert_iadd(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ladd(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_fadd(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_dadd(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_isub(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lsub(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_fsub(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_dsub(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_imul(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lmul(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_fmul(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_dmul(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_idiv(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ldiv(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_fdiv(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ddiv(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_irem(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lrem(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_frem(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_drem(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ineg(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lneg(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_fneg(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_dneg(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ishl(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lshl(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ishr(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lshr(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_iand(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_land(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ior(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lor(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_ixor(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lxor(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_iinc(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_lcmp(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_xcmpl(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_xcmpg(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_xcmpl(struct compilation_unit *, struct basic_block *, unsigned long);
-extern int convert_xcmpg(struct compilation_unit *, struct basic_block *, unsigned long);
-
-#endif
+DECLARE_CONVERTER(convert_iadd)
+DECLARE_CONVERTER(convert_ladd)
+DECLARE_CONVERTER(convert_fadd)
+DECLARE_CONVERTER(convert_dadd)
+DECLARE_CONVERTER(convert_isub)
+DECLARE_CONVERTER(convert_lsub)
+DECLARE_CONVERTER(convert_fsub)
+DECLARE_CONVERTER(convert_dsub)
+DECLARE_CONVERTER(convert_imul)
+DECLARE_CONVERTER(convert_lmul)
+DECLARE_CONVERTER(convert_fmul)
+DECLARE_CONVERTER(convert_dmul)
+DECLARE_CONVERTER(convert_idiv)
+DECLARE_CONVERTER(convert_ldiv)
+DECLARE_CONVERTER(convert_fdiv)
+DECLARE_CONVERTER(convert_ddiv)
+DECLARE_CONVERTER(convert_irem)
+DECLARE_CONVERTER(convert_lrem)
+DECLARE_CONVERTER(convert_frem)
+DECLARE_CONVERTER(convert_drem)
+DECLARE_CONVERTER(convert_ineg)
+DECLARE_CONVERTER(convert_lneg)
+DECLARE_CONVERTER(convert_fneg)
+DECLARE_CONVERTER(convert_dneg)
+DECLARE_CONVERTER(convert_ishl)
+DECLARE_CONVERTER(convert_lshl)
+DECLARE_CONVERTER(convert_ishr)
+DECLARE_CONVERTER(convert_lshr)
+DECLARE_CONVERTER(convert_iand)
+DECLARE_CONVERTER(convert_land)
+DECLARE_CONVERTER(convert_ior)
+DECLARE_CONVERTER(convert_lor)
+DECLARE_CONVERTER(convert_ixor)
+DECLARE_CONVERTER(convert_lxor)
+DECLARE_CONVERTER(convert_iinc)
+DECLARE_CONVERTER(convert_lcmp)
+DECLARE_CONVERTER(convert_xcmpl)
+DECLARE_CONVERTER(convert_xcmpg)
+DECLARE_CONVERTER(convert_xcmpl)
+DECLARE_CONVERTER(convert_xcmpg)
