155e50ef16bb561907efd982bb73a9001f89c51a
Pekka Enberg <penberg@haji.ri.fi>
2006-07-06 15:07:57 +0300

    [PATCH] Merge disassembler from CACAO
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

---
+++ jit/disass-common.c
@@ -0,0 +1,102 @@
+/* src/vm/jit/disass-common.c - common functions for GNU binutils disassembler
+
+   Copyright (C) 1996-2005, 2006 R. Grafl, A. Krall, C. Kruegel,
+   C. Oates, R. Obermaisser, M. Platter, M. Probst, S. Ring,
+   E. Steiner, C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich,
+   J. Wenninger, Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+   Contact: cacao@cacaojvm.org
+
+   Authors: Christian Thalinger
+
+   Changes:
+
+*/
+
+#include "disass.h"
+
+#include <dis-asm.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+/* global variables ***********************************************************/
+
+disassemble_info info;
+bool disass_initialized = false;
+
+
+/* We need this on i386 and x86_64 since we don't know the byte length
+   of currently printed instructions.  512 bytes should be enough. */
+
+char disass_buf[512];
+unsigned long disass_len;
+
+
+/* disassemble *****************************************************************
+
+   Outputs a disassembler listing of some machine code on `stdout'.
+
+   start: pointer to first machine instruction
+   end:   pointer after last machine instruction
+
+*******************************************************************************/
+
+void disassemble(void *start, void *end)
+{
+	printf("  --- disassembler listing ---\n");
+
+	for (; start < end; )
+		start = disassinstr(start);
+}
+
+
+/* disass_printf ***************************************************************
+
+   Required by binutils disassembler.  This just prints the
+   disassembled instructions to stdout.
+
+*******************************************************************************/
+
+void disass_printf(PTR p, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+
+	disass_len += vsprintf(disass_buf + disass_len, fmt, ap);
+
+	va_end(ap);
+}
+
+
+/* buffer_read_memory **********************************************************
+
+   We need to replace the buffer_read_memory from binutils.
+
+*******************************************************************************/
+
+int disass_buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, unsigned int length, struct disassemble_info *info)
+{
+	memcpy(myaddr, (void *) (unsigned long) memaddr, length);
+
+	return 0;
+}
---
+++ jit/disass.h
@@ -0,0 +1,72 @@
+/* src/vm/jit/disass.h - disassembler header
+
+   Copyright (C) 1996-2005, 2006 R. Grafl, A. Krall, C. Kruegel,
+   C. Oates, R. Obermaisser, M. Platter, M. Probst, S. Ring,
+   E. Steiner, C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich,
+   J. Wenninger, Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+   Contact: cacao@cacaojvm.org
+
+   Authors: Christian Thalinger
+
+   Changes:
+
+*/
+
+
+#ifndef _DISASS_H
+#define _DISASS_H
+
+#include <dis-asm.h>
+#include <stdbool.h>
+
+/* some macros ****************************************************************/
+
+#define DISASSINSTR(code) \
+    (code) = disassinstr((code))
+
+#define DISASSEMBLE(start,end) \
+    disassemble((start), (end))
+
+
+/* export global variables ****************************************************/
+
+extern disassemble_info info;
+extern bool disass_initialized;
+
+extern char *regs[];
+
+extern char disass_buf[512];
+extern unsigned long disass_len;
+
+
+/* function prototypes *******************************************************/
+
+void disassemble(void *start, void *end);
+
+void disass_printf(PTR p, const char *fmt, ...);
+
+int disass_buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, unsigned int length, struct disassemble_info *info);
+
+/* machine dependent functions */
+
+unsigned char *disassinstr(unsigned char *code);
+
+#endif /* _DISASS_H */
