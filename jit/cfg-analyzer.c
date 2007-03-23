91f222994a7cd5895c4a554d1baa2b599c40d598
Pekka Enberg <penberg@cs.helsinki.fi>
2007-03-23 16:03:56 +0200

    cfg: code cleanups
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- jit/cfg-analyzer.c
+++ jit/cfg-analyzer.c
@@ -34,61 +34,6 @@ static void update_branch_successors(struct compilation_unit *cu)
 	}
 }
 
-static unsigned char *bytecode_next_insn(struct stream *stream)
-{
-	unsigned long opc_size;
-		
-	opc_size = bc_insn_size(stream->current);
-	assert(opc_size != 0);
-	return stream->current + opc_size;
-}
-
-static struct stream_operations bytecode_stream_ops = {
-	.new_position = bytecode_next_insn,
-};
-
-static struct basic_block *do_split(struct compilation_unit *cu,
-				    struct basic_block *bb,
-				    struct stream *stream,
-				    struct bitset *branch_targets)
-{
-	unsigned long current_offset;
-	unsigned long br_target_off;
-	struct basic_block *new_bb;
-
-	current_offset = stream_offset(stream);
-	br_target_off = bc_target_off(stream->current) + current_offset;
-
-	set_bit(branch_targets->bits, br_target_off);
-	bb->br_target_off = br_target_off;
-	bb->has_branch = true;
-	new_bb = bb_split(bb, current_offset + bc_insn_size(stream->current));
-
-	if (!bc_is_goto(*stream->current))
-		bb->successors[0] = new_bb;
-
-	return new_bb;
-}
-
-static void split_after_branches(struct compilation_unit *cu,
-				 struct basic_block *entry_bb,
-				 struct bitset *branch_targets)
-{
-	struct basic_block *bb;
-	struct stream stream;
-
-	stream_init(&stream, cu->method->jit_code, cu->method->code_size, &bytecode_stream_ops);
-
-	bb = entry_bb;
-
-	while (stream_has_more(&stream)) {
-		if (bc_is_branch(*stream.current))
-			bb = do_split(cu, bb, &stream, branch_targets);
-
-		stream_advance(&stream);
-	}
-}
-
 static void split_at_branch_targets(struct compilation_unit *cu,
 				    struct bitset *branch_targets)
 {
@@ -111,18 +56,75 @@ static void split_at_branch_targets(struct compilation_unit *cu,
 	}
 }
 
+static void split_after_branches(struct stream *stream,
+				 struct basic_block *entry_bb,
+				 struct bitset *branch_targets)
+{
+	struct basic_block *bb;
+
+	bb = entry_bb;
+
+	for (; stream_has_more(stream); stream_advance(stream)) {
+		unsigned long offset, next_insn_off;
+		unsigned long br_target_off;
+		struct basic_block *new_bb;
+		unsigned char *code;
+
+		code = stream->current;
+
+		if (!bc_is_branch(*code))
+			continue;
+
+		offset = stream_offset(stream);
+		br_target_off = bc_target_off(code) + offset;
+		next_insn_off = offset + bc_insn_size(code);
+
+		new_bb = bb_split(bb, next_insn_off);
+		if (!bc_is_goto(*code))
+			bb->successors[0] = new_bb;
+
+		bb->br_target_off = br_target_off;
+		bb->has_branch = true;
+
+		set_bit(branch_targets->bits, br_target_off);
+
+		bb = new_bb;
+	}
+}
+
+static unsigned char *bytecode_next_insn(struct stream *stream)
+{
+	unsigned long opc_size;
+		
+	opc_size = bc_insn_size(stream->current);
+	assert(opc_size != 0);
+	return stream->current + opc_size;
+}
+
+static struct stream_operations bytecode_stream_ops = {
+	.new_position = bytecode_next_insn,
+};
+
+static void bytecode_stream_init(struct stream *stream, struct methodblock *method)
+{
+	stream_init(stream, method->jit_code, method->code_size,
+		    &bytecode_stream_ops);
+}
+
 int analyze_control_flow(struct compilation_unit *cu)
 {
 	struct bitset *branch_targets;
 	struct basic_block *entry_bb;
+	struct stream stream;
 
 	branch_targets = alloc_bitset(cu->method->code_size);
 	if (!branch_targets)
 		return -ENOMEM;
 
-	entry_bb = get_basic_block(cu, 0, cu->method->code_size);
+	bytecode_stream_init(&stream, cu->method);
 
-	split_after_branches(cu, entry_bb, branch_targets);
+	entry_bb = get_basic_block(cu, 0, cu->method->code_size);
+	split_after_branches(&stream, entry_bb, branch_targets);
 	split_at_branch_targets(cu, branch_targets);
 	update_branch_successors(cu);
 
