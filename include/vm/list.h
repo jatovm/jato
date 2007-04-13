fce5c16a2201fb459bd52464c554ba979afe8a80
Pekka Enberg <penberg@cs.helsinki.fi>
2007-04-13 16:14:01 +0300

    vm: add list_for_each_entry_reverse
    This is needed for iterating basic blocks in reverse order for liveness
    analysis.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/vm/list.h
+++ include/vm/list.h
@@ -116,6 +116,17 @@ static inline bool list_is_empty(struct list_head *head)
              pos = list_entry(pos->member.next, typeof(*pos), member))
 
 /**
+ * list_for_each_entry_reverse - iterate backwards over list of given type.
+ * @pos:        the type * to use as a loop cursor.
+ * @head:       the head for your list.
+ *  @member:     the name of the list_struct within the struct.
+ */
+#define list_for_each_entry_reverse(pos, head, member)                  \
+	for (pos = list_entry((head)->prev, typeof(*pos), member);      \
+	     &pos->member != (head);                                    \
+	     pos = list_entry(pos->member.prev, typeof(*pos), member))
+
+/**
  * list_for_each_entry_safe - iterate over list of given type safe against removal of list entry
  * @pos:        the type * to use as a loop counter.
  * @n:          another type * to use as temporary storage
