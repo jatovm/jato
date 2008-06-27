d0f5409ced5025e96f63fd0a1cde3827526e70b6
Arthur HUILLET <arthur.huillet@free.fr>
2008-06-27 10:13:08 +0200

    Properly convert DUP2, DUP2_X1, DUP2_X2 to HIR.
    Signed-off-by: Arthur HUILLET <arthur.huillet@free.fr>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- jit/ostack-bc.c
+++ jit/ostack-bc.c
@@ -47,28 +47,56 @@ static struct expression *dup_expr(struct parse_context *ctx, struct expression 
 	return dest;
 }
 
-int convert_dup(struct parse_context *ctx)
+static int __convert_dup(struct parse_context *ctx, struct expression *value)
 {
-	struct expression *dup, *value;
+	struct expression *dup;
 
-	value = stack_pop(ctx->cu->expr_stack);
 	dup = dup_expr(ctx, value);
 
 	stack_push(ctx->cu->expr_stack, dup);
 	stack_push(ctx->cu->expr_stack, dup);
+	return 0;
+}
 
+int convert_dup(struct parse_context *ctx)
+{
+	struct expression *value;
+
+	value = stack_pop(ctx->cu->expr_stack);
+
+	return __convert_dup(ctx, value);
+}
+
+static int __convert_dup_x1(struct parse_context *ctx, struct expression *value1, struct expression *value2)
+{
+	struct expression *dup;
+
+	dup = dup_expr(ctx, value1);
+
+	stack_push(ctx->cu->expr_stack, dup);
+	stack_push(ctx->cu->expr_stack, value2);
+	stack_push(ctx->cu->expr_stack, dup);
 	return 0;
 }
 
 int convert_dup_x1(struct parse_context *ctx)
 {
-	struct expression *value1, *value2, *dup;
+	struct expression *value1, *value2;
 
 	value1 = stack_pop(ctx->cu->expr_stack);
 	value2 = stack_pop(ctx->cu->expr_stack);
+
+	return __convert_dup_x1(ctx, value1, value2);
+}
+
+static int __convert_dup_x2(struct parse_context *ctx, struct expression *value1, struct expression *value2, struct expression *value3)
+{
+	struct expression *dup;
+
 	dup = dup_expr(ctx, value1);
 
 	stack_push(ctx->cu->expr_stack, dup);
+	stack_push(ctx->cu->expr_stack, value3);
 	stack_push(ctx->cu->expr_stack, value2);
 	stack_push(ctx->cu->expr_stack, dup);
 
@@ -77,18 +105,122 @@ int convert_dup_x1(struct parse_context *ctx)
 
 int convert_dup_x2(struct parse_context *ctx)
 {
-	struct expression *value1, *value2, *value3, *dup;
+	struct expression *value1, *value2, *value3;
 
 	value1 = stack_pop(ctx->cu->expr_stack);
 	value2 = stack_pop(ctx->cu->expr_stack);
 	value3 = stack_pop(ctx->cu->expr_stack);
-	dup = dup_expr(ctx, value1);
 
+	return __convert_dup_x2(ctx, value1, value2, value3);
+}
+
+static int __convert_dup2(struct parse_context *ctx, struct expression *value1, struct expression *value2)
+{
+	struct expression *dup, *dup2;
+	dup = dup_expr(ctx, value1);
+	dup2 = dup_expr(ctx, value2);
+
+	stack_push(ctx->cu->expr_stack, dup2);
+	stack_push(ctx->cu->expr_stack, dup);
+	stack_push(ctx->cu->expr_stack, dup2);
+	stack_push(ctx->cu->expr_stack, dup);
+
+	return 0;
+}
+
+int convert_dup2(struct parse_context *ctx)
+{
+	struct expression *value, *value2;
+	/* Dup2 duplicates *one* 64bit operand or *two* 32 bit operands.
+	 * This second case is to be handled here. */
+	value = stack_pop(ctx->cu->expr_stack);
+
+	if (value->vm_type == J_LONG || value->vm_type == J_DOUBLE) {
+		return __convert_dup(ctx, value);
+	} else {
+		value2 = stack_pop(ctx->cu->expr_stack);
+		return __convert_dup2(ctx, value, value2);
+	}
+}
+
+static int __convert_dup2_x1(struct parse_context * ctx, struct expression * value1, struct expression *value2, struct expression *value3)
+{
+	struct expression *dup, *dup2;
+
+	dup = dup_expr(ctx, value1);
+	dup2 = dup_expr(ctx, value2);
+
+	stack_push(ctx->cu->expr_stack, dup2);
 	stack_push(ctx->cu->expr_stack, dup);
 	stack_push(ctx->cu->expr_stack, value3);
-	stack_push(ctx->cu->expr_stack, value2);
+	stack_push(ctx->cu->expr_stack, dup2);
 	stack_push(ctx->cu->expr_stack, dup);
 
+	return 0;
+}
+
+int convert_dup2_x1(struct parse_context *ctx)
+{
+	struct expression *value1, *value2, *value3;
+	/* Dup2 duplicates *one* 64bit operand or *two* 32 bit operands.
+	 * This second case is to be handled here. */
+	value1 = stack_pop(ctx->cu->expr_stack);
+	value2 = stack_pop(ctx->cu->expr_stack);
+
+	if (value1->vm_type == J_LONG || value1->vm_type == J_DOUBLE) {
+		return __convert_dup_x1(ctx, value1, value2);
+	} else {
+		value3 = stack_pop(ctx->cu->expr_stack);
+		return __convert_dup2_x1(ctx, value1, value2, value3);
+	}
+}
+
+static int __convert_dup2_x2(struct parse_context *ctx, struct expression *value1, struct expression *value2, struct expression *value3, struct expression *value4)
+{
+	struct expression *dup, *dup2;
+	dup = dup_expr(ctx, value1);
+	dup2 = dup_expr(ctx, value2);
+
+	stack_push(ctx->cu->expr_stack, dup2);
+	stack_push(ctx->cu->expr_stack, dup);
+	stack_push(ctx->cu->expr_stack, value4);
+	stack_push(ctx->cu->expr_stack, value3);
+	stack_push(ctx->cu->expr_stack, dup2);
+	stack_push(ctx->cu->expr_stack, dup);
+
+	return 0;
+}
+
+int convert_dup2_x2(struct parse_context *ctx)
+{
+	struct expression *value1, *value2, *value3, *value4;
+
+	/* Refer to the JVM spec at http://java.sun.com/docs/books/jvms/second_edition/html/Instructions2.doc3.html#dup2_x2
+	 */
+	value1 = stack_pop(ctx->cu->expr_stack);
+	value2 = stack_pop(ctx->cu->expr_stack);
+
+	if (value1->vm_type == J_LONG || value1->vm_type == J_DOUBLE) {
+		if (value2->vm_type == J_LONG || value2->vm_type == J_DOUBLE) {
+			/* Call dup_x1 - form 4*/
+			return __convert_dup_x1(ctx, value1, value2);
+		} else {
+			/* Call dup_x2 - form 2*/
+			value3 = stack_pop(ctx->cu->expr_stack);
+			return __convert_dup_x2(ctx, value1, value2, value3);
+		}
+	} else {
+		value3 = stack_pop(ctx->cu->expr_stack);
+
+		if (value3->vm_type == J_LONG || value3->vm_type == J_DOUBLE) {
+			/* Call dup2_x1 - form 3*/
+			return __convert_dup2_x1(ctx, value1, value2, value3);
+		} else {
+			/* the annoying case - form 1 */
+			value4 = stack_pop(ctx->cu->expr_stack);
+			return __convert_dup2_x2(ctx, value1, value2, value3, value4);
+		}
+	}
 	return 0;
 }
 
--- test/jit/ostack-bc-test.c
+++ test/jit/ostack-bc-test.c
@@ -90,14 +90,58 @@ static void assert_dup_stack(unsigned char opc, struct expression *value)
 	free_simple_method(method);
 }
 
+static void assert_dup2_stack(unsigned char opc, struct expression *value, struct expression *value2)
+{
+	struct compilation_unit *cu;
+	struct methodblock *method;
+	struct statement *stmt, *stmt2;
+
+	if (value->vm_type == J_LONG || value->vm_type == J_DOUBLE) {
+		assert_dup_stack(opc, value);
+		return;
+	}
+
+	method = alloc_simple_method(opc);
+	cu = method->compilation_unit;
+
+	stack_push(cu->expr_stack, value2);
+	stack_push(cu->expr_stack, value);
+
+	convert_to_ir(cu);
+	stmt = stmt_entry(bb_entry(cu->bb_list.next)->stmt_list.next);
+	stmt2 = stmt_entry(bb_entry(cu->bb_list.next)->stmt_list.next->next);
+
+	assert_store_stmt(stmt);
+	assert_ptr_equals(value, to_expr(stmt->store_src));
+	assert_temporary_expr(0, stmt->store_dest);
+
+	assert_store_stmt(stmt2);
+	assert_ptr_equals(value2, to_expr(stmt2->store_src));
+	assert_temporary_expr(0, stmt->store_dest);
+
+	assert_ptr_equals(to_expr(stmt->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt2->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt2->store_dest), stack_pop(cu->expr_stack));
+
+	assert_true(stack_is_empty(cu->expr_stack));
+
+	free_simple_method(method);
+}
+
 void test_convert_dup(void)
 {
 	struct expression *value = value_expr(J_REFERENCE, 0xdeadbeef);
+	struct expression *value2 = value_expr(J_REFERENCE, 0xcafedeca);
+	struct expression *value3 = value_expr(J_LONG, 0xcafecafecafecafe);
 
 	expr_get(value);
+	expr_get(value2);
+	expr_get(value3);
 
 	assert_dup_stack(OPC_DUP, value);
-	assert_dup_stack(OPC_DUP2, value);
+	assert_dup2_stack(OPC_DUP2, value, value2);
+	assert_dup2_stack(OPC_DUP2, value3, NULL);
 }
 
 static void assert_dup_x1_stack(unsigned char opc, struct expression *value1,
@@ -129,24 +173,65 @@ static void assert_dup_x1_stack(unsigned char opc, struct expression *value1,
 	free_simple_method(method);
 }
 
+static void assert_dup2_x1_stack(unsigned char opc, struct expression *value1,
+				struct expression *value2, struct expression *value3)
+{
+	struct compilation_unit *cu;
+	struct methodblock *method;
+	struct statement *stmt, *stmt2;
+
+	if (value1->vm_type == J_LONG || value2->vm_type == J_DOUBLE) {
+		assert_dup_x1_stack(opc, value1, value2);
+		return;
+	}
+
+	method = alloc_simple_method(opc);
+	cu = method->compilation_unit;
+
+	stack_push(cu->expr_stack, value3);
+	stack_push(cu->expr_stack, value2);
+	stack_push(cu->expr_stack, value1);
+
+	convert_to_ir(cu);
+        stmt = stmt_entry(bb_entry(cu->bb_list.next)->stmt_list.next);
+	stmt2 = stmt_entry(bb_entry(cu->bb_list.next)->stmt_list.next->next);
+
+	assert_store_stmt(stmt);
+	assert_ptr_equals(value1, to_expr(stmt->store_src));
+	assert_temporary_expr(0, stmt->store_dest);
+
+	assert_store_stmt(stmt2);
+	assert_ptr_equals(value2, to_expr(stmt2->store_src));
+	assert_temporary_expr(0, stmt2->store_dest);
+
+	assert_ptr_equals(to_expr(stmt->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt2->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(value3, stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt2->store_dest), stack_pop(cu->expr_stack));
+
+	assert_true(stack_is_empty(cu->expr_stack));
+
+	free_simple_method(method);
+}
+
 void test_convert_dup_x1(void)
 {
-	struct expression *value1, *value2;
+	struct expression *value1, *value2, *value3;
 
 	value1 = value_expr(J_REFERENCE, 0xdeadbeef);
 	value2 = value_expr(J_REFERENCE, 0xcafebabe);
+	value3 = value_expr(J_LONG, 0xdecacafebabebeef);
 
 	expr_get(value1);
 
 	assert_dup_x1_stack(OPC_DUP_X1, value1, value2);
-	assert_dup_x1_stack(OPC_DUP2_X1, value1, value2);
-
-	expr_put(value2);
+	assert_dup2_x1_stack(OPC_DUP2_X1, value1, value2, value3);
+	assert_dup2_x1_stack(OPC_DUP2_X1, value3, value2, value1);
 }
 
 static void assert_dup_x2_stack(unsigned char opc, struct expression *value1,
-				struct expression *value2,
-				struct expression *value3)
+				struct expression *value2, struct expression *value3)
 {
 	struct compilation_unit *cu;
 	struct methodblock *method;
@@ -176,21 +261,75 @@ static void assert_dup_x2_stack(unsigned char opc, struct expression *value1,
 	free_simple_method(method);
 }
 
+static void assert_dup2_x2_stack(unsigned char opc, struct expression *value1,
+				struct expression *value2, struct expression *value3,
+				struct expression *value4)
+{
+	struct compilation_unit *cu;
+	struct methodblock *method;
+	struct statement *stmt, *stmt2;
+
+	if (value1->vm_type == J_LONG || value1->vm_type == J_DOUBLE) {
+		if (value2->vm_type == J_LONG || value2->vm_type == J_DOUBLE) {
+			assert_dup_x1_stack(opc, value1, value2);
+			return;
+		} else {
+			assert_dup_x2_stack(opc, value1, value2, value3);
+			return;
+		}
+	} else {
+		if (value3->vm_type == J_LONG || value3->vm_type == J_DOUBLE) {
+			assert_dup2_x1_stack(opc, value1, value2, value3);
+			return;
+		}
+	}
+
+	method = alloc_simple_method(opc);
+	cu = method->compilation_unit;
+
+	stack_push(cu->expr_stack, value4);
+	stack_push(cu->expr_stack, value3);
+	stack_push(cu->expr_stack, value2);
+	stack_push(cu->expr_stack, value1);
+
+	convert_to_ir(cu);
+        stmt = stmt_entry(bb_entry(cu->bb_list.next)->stmt_list.next);
+	stmt2 = stmt_entry(bb_entry(cu->bb_list.next)->stmt_list.next->next);
+
+	assert_store_stmt(stmt);
+	assert_ptr_equals(value1, to_expr(stmt->store_src));
+	assert_temporary_expr(0, stmt->store_dest);
+
+	assert_store_stmt(stmt2);
+	assert_ptr_equals(value2, to_expr(stmt2->store_src));
+	assert_temporary_expr(0, stmt2->store_dest);
+
+	assert_ptr_equals(to_expr(stmt->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt2->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(value3, stack_pop(cu->expr_stack));
+	assert_ptr_equals(value4, stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt->store_dest), stack_pop(cu->expr_stack));
+	assert_ptr_equals(to_expr(stmt2->store_dest), stack_pop(cu->expr_stack));
+
+	assert_true(stack_is_empty(cu->expr_stack));
+
+	free_simple_method(method);
+}
+
 void test_convert_dup_x2(void)
 {
-	struct expression *value1, *value2, *value3;
+	struct expression *value1, *value2, *value3, *value4;
 
 	value1 = value_expr(J_REFERENCE, 0xdeadbeef);
 	value2 = value_expr(J_REFERENCE, 0xcafebabe);
 	value3 = value_expr(J_REFERENCE, 0xb4df00d);
+	value4 = value_expr(J_REFERENCE, 0x6559570);
 
 	expr_get(value1);
 
 	assert_dup_x2_stack(OPC_DUP_X2, value1, value2, value3);
-	assert_dup_x2_stack(OPC_DUP2_X2, value1, value2, value3);
+	assert_dup2_x2_stack(OPC_DUP2_X2, value1, value2, value3, value4);
 
-	expr_put(value2);
-	expr_put(value3);
 }
 
 static void assert_swap_stack(unsigned char opc,
