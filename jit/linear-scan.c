1a012018b14d88ba27d0cee184429fa1c40b1c45
Pekka Enberg <penberg@cs.helsinki.fi>
2008-07-16 15:41:25 +0300

    jit: check for buffer overflow in try_to_allocate_free_reg()
    As reported by Arthur Huillet, if the it->reg value is bogus, we can get nasty
    corruption for the 'active' list. Debugging that is not straight-forward so add
    an assertion to catch the error early.
    
    Reported-by: Arthur HUILLET <arthur.huillet@free.fr>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- jit/linear-scan.c
+++ jit/linear-scan.c
@@ -76,6 +76,7 @@ static void try_to_allocate_free_reg(struct live_interval *current,
 		free_until_pos[i] = LONG_MAX;
 
 	list_for_each_entry(it, active, interval_node) {
+		assert(it->reg < NR_REGISTERS);
 		free_until_pos[it->reg] = 0;
 	}
 
