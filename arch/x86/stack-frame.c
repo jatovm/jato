175661472a51b927c5a1db561773e3cc9b9fbc49
Pekka Enberg <penberg@cs.helsinki.fi>
2008-08-18 12:26:45 +0300

    x86: use index_to_offset() in frame_local_offset()
    Make frame_local_offset() more readable and remove some duplicate code.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- arch/x86/stack-frame.c
+++ arch/x86/stack-frame.c
@@ -1,11 +1,27 @@
 /*
- * Copyright (C) 2006  Pekka Enberg
+ * Copyright (C) 2006-2008  Pekka Enberg
  *
- * This file is released under the GPL version 2. Please refer to the file
- * LICENSE for details.
+ * This file is released under the GPL version 2 with the following
+ * clarification and special exception:
  *
- * The file contains functions for managing the native IA-32 function stack
- * frame.
+ *     Linking this library statically or dynamically with other modules is
+ *     making a combined work based on this library. Thus, the terms and
+ *     conditions of the GNU General Public License cover the whole
+ *     combination.
+ *
+ *     As a special exception, the copyright holders of this library give you
+ *     permission to link this library with independent modules to produce an
+ *     executable, regardless of the license terms of these independent
+ *     modules, and to copy and distribute the resulting executable under terms
+ *     of your choice, provided that you also meet, for each linked independent
+ *     module, the terms and conditions of the license of that module. An
+ *     independent module is a module which is not derived from or based on
+ *     this library. If you modify this library, you may extend this exception
+ *     to your version of the library, but you are not obligated to do so. If
+ *     you do not wish to do so, delete this exception statement from your
+ *     version.
+ *
+ * Please refer to the file LICENSE for details.
  */
 
 #include <jit/expression.h>
@@ -15,6 +31,11 @@
 
 #define ARGS_START_OFFSET (sizeof(unsigned long) * 5)
 
+static unsigned long index_to_offset(unsigned long index)
+{
+	return index * sizeof(unsigned int);
+}
+
 long frame_local_offset(struct methodblock *method, struct expression *local)
 {
 	unsigned long idx, nr_args;
@@ -23,14 +44,9 @@ long frame_local_offset(struct methodblock *method, struct expression *local)
 	nr_args = method->args_count;
 
 	if (idx < nr_args)
-		return (idx * sizeof(u4)) + ARGS_START_OFFSET;
+		return ARGS_START_OFFSET + index_to_offset(idx);
 
-	return 0 - ((idx - nr_args+1) * sizeof(u4));
-}
-
-static unsigned long index_to_offset(unsigned long index)
-{
-	return index * sizeof(unsigned int);
+	return 0 - index_to_offset(idx - nr_args + 1);
 }
 
 unsigned long slot_offset(struct stack_slot *slot)
