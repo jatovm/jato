450d730485e4b5e0675e5201e4a3963d80d93ef1
Pekka Enberg <penberg@cs.helsinki.fi>
2008-08-20 12:43:27 +0300

    x86: clean up emit-code.h
    Fix up include guards and remove an unnecessary include.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/arch-x86/emit-code.h
+++ include/arch-x86/emit-code.h
@@ -1,15 +1,13 @@
-#ifndef __JIT_X86_OBJCODE_H
-#define __JIT_X86_OBJCODE_H
+#ifndef __X86_EMIT_CODE_H
+#define __X86_EMIT_CODE_H
 
-#include <arch/instruction.h>
-
-struct buffer;
-struct basic_block;
 struct compilation_unit;
+struct basic_block;
+struct buffer;
 
 void emit_prolog(struct buffer *, unsigned long);
 void emit_epilog(struct buffer *, unsigned long);
 void emit_body(struct basic_block *, struct buffer *);
 void emit_trampoline(struct compilation_unit *, void *, struct buffer *);
     
-#endif
+#endif /* __X86_EMIT_CODE */
