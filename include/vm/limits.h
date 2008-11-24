8fc72cb0f85e7b7fa44cfc1309f32423143dc631
Pekka Enberg <penberg@cs.helsinki.fi>
2008-11-24 09:49:26 +0200

    vm: use <stdint.h> limits instead of open-coding them
    As suggested by Tuomas Kärkkäinen, use the INT8_MIN et al constants from
    <stdint.h> instead of open-coding them.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/vm/limits.h
+++ include/vm/limits.h
@@ -9,19 +9,19 @@
  */
 
 /* For byte, from -128 to 127 (-2^7 to 2^7-1), inclusive */
-#define J_BYTE_MIN ((int8_t)-(1ULL << 7)     )
-#define J_BYTE_MAX ((int8_t)((1ULL << 7) - 1))
+#define J_BYTE_MIN INT8_MIN
+#define J_BYTE_MAX INT8_MAX
 
 /* For short, from -32768 to 32767 (-2^15 to 2^15-1), inclusive */
-#define J_SHORT_MIN ((int16_t)-(1ULL << 15)     )
-#define J_SHORT_MAX ((int16_t)((1ULL << 15) - 1))
+#define J_SHORT_MIN INT16_MIN
+#define J_SHORT_MAX INT16_MAX
 
 /* For int, from -2147483648 to 2147483647 (-2^31 to 2^31-1), inclusive */
-#define J_INT_MIN ((int32_t)-(1ULL << 31)     )
-#define J_INT_MAX ((int32_t)((1ULL << 31) - 1))
+#define J_INT_MIN INT32_MIN
+#define J_INT_MAX INT32_MAX
 
 /* For long, from -9223372036854775808 to 9223372036854775807 (-2^63 to 2^63-1), inclusive */
-#define J_LONG_MIN ((int64_t)-(1ULL << 63)     )
-#define J_LONG_MAX ((int64_t)((1ULL << 63) - 1))
+#define J_LONG_MIN INT64_MIN
+#define J_LONG_MAX INT64_MAX
 
 #endif /* VM_LIMITS_H */
