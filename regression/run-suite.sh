53bae6a7b23bcc9d398ea7dc11348230a0c0ff09
Arthur HUILLET <arthur.huillet@free.fr>
2009-01-24 16:41:41 +0100

    regression: Add cmdline options regression testing script
    -t enables tracing
    
    class names specify which classes to run, otherwise the script runs the whole
    suite
    
    Signed-off-by: Arthur HUILLET <arthur.huillet@free.fr>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- regression/run-suite.sh
+++ regression/run-suite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HAS_FAILURES=0
-JAVA_OPTS=$*
+CLASS_LIST=""
 
 function run_java {
   JAVA_CLASS=$1
@@ -29,16 +29,43 @@ GLIBJ=$GNU_CLASSPATH_ROOT/share/classpath/glibj.zip
 
 BOOTCLASSPATH=../lib/classes.zip:$GLIBJ
 
-run_java jamvm.ExitStatusIsZeroTest 0
-run_java jamvm.ExitStatusIsOneTest 1
-run_java jamvm.LoadConstantsTest 0
-run_java jamvm.IntegerArithmeticTest 0
-run_java jamvm.LongArithmeticTest 0
-run_java jamvm.ConversionTest 0
-run_java jamvm.ObjectCreationAndManipulationTest 0
-run_java jamvm.SynchronizationTest 0
-run_java jamvm.MethodInvocationAndReturnTest 0
-run_java jamvm.ControlTransferTest 0
+while [ "$#" -ge 1 ]; do
+    case "$1" in 
+	-t)
+	echo 'Tracing execution.'
+	JAVA_OPTS="$JAVA_OPTS -Xtrace:jit"
+	;;
+
+	-*)
+	JAVA_OPTS="$JAVA_OPTS $1"
+	;;
+
+	*)
+	echo 'Running test' $1
+	CLASS_LIST="$CLASS_LIST $1"
+	;;
+    esac;
+
+    shift
+done
+
+if [ -z "$CLASS_LIST" ]; then
+    run_java jamvm.ExitStatusIsZeroTest 0
+    run_java jamvm.ExitStatusIsOneTest 1
+    run_java jamvm.LoadConstantsTest 0
+    run_java jamvm.IntegerArithmeticTest 0
+    run_java jamvm.LongArithmeticTest 0
+    run_java jamvm.ConversionTest 0
+    run_java jamvm.ObjectCreationAndManipulationTest 0
+    run_java jamvm.SynchronizationTest 0
+    run_java jamvm.MethodInvocationAndReturnTest 0
+    run_java jamvm.ControlTransferTest 0
+else 
+    for i in $CLASS_LIST; do
+	run_java $i 0
+    done
+fi
+
 
 if [ "$HAS_FAILURES" == "0" ]; then
   echo "Tests OK."
