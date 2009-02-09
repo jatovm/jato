c5ac15fc8118a2d59c846a5a6e74351ad771f6d7
Pekka Enberg <penberg@cs.helsinki.fi>
2009-02-09 21:34:55 +0200

    powerpc: Fix compiler warnings in instruction.h
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/arch-ppc/instruction.h
+++ include/arch-ppc/instruction.h
@@ -5,12 +5,16 @@
 #include <vm/list.h>
 #include <arch/registers.h>
 
+struct var_info;
+
 struct insn {
 	unsigned long lir_pos;
         struct list_head insn_list_node;
 };
 
 #define for_each_insn(insn, insn_list) list_for_each_entry(insn, insn_list, insn_list_node)
+
+void free_insn(struct insn *);
 
 static inline bool insn_defs(struct insn *insn, struct var_info *var)
 {
