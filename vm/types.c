55770779f2f5e56ed32ed58eb3a8d6c443c56a42
Vegard Nossum <vegard.nossum@gmail.com>
2009-05-18 19:23:59 +0200

    vm: added function for parsing type descriptors
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- vm/types.c
+++ vm/types.c
@@ -36,3 +36,92 @@ enum vm_type get_method_return_type(char *type)
 
 	return str_to_type(type + 1);
 }
+
+#include <stdio.h>
+
+static unsigned int count_skip_arguments(const char **type);
+
+static int skip_type(const char **type)
+{
+	const char *ptr = *type;
+
+	switch (*ptr) {
+	/* BaseType */
+	case 'B':
+	case 'C':
+	case 'D':
+	case 'F':
+	case 'I':
+	case 'J':
+	case 'S':
+	case 'Z':
+		++ptr;
+		break;
+
+		/* ObjectType */
+	case 'L':
+		while (1) {
+			if (!*ptr)
+				return -1;
+			if (*ptr == ';')
+				break;
+
+			++ptr;
+		}
+
+		++ptr;
+		break;
+
+		/* ArrayType */
+	case '[':
+		++ptr;
+		if (skip_type(&ptr))
+			return -1;
+		break;
+
+	default:
+		NOT_IMPLEMENTED;
+		return -1;
+	}
+
+	*type = ptr;
+	return 0;
+}
+
+static unsigned int count_skip_arguments(const char **type)
+{
+	unsigned int args = 0;
+	const char *ptr = *type;
+
+	while (1) {
+		if (!*ptr)
+			break;
+
+		if (*ptr == ')')
+			break;
+
+		if (skip_type(&ptr))
+			return -1;
+
+		++args;
+	}
+
+	*type = ptr;
+	return args;
+}
+
+unsigned int count_arguments(const char *type)
+{
+	unsigned int args = 0;
+
+	if (*type != '(')
+		return -1;
+	++type;
+
+	args = count_skip_arguments(&type);
+
+	if (*type != ')')
+		return -1;
+
+	return args;
+}
