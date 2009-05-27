07f1b9ea6cd2e721a38d94059c5c491ab8e71bb5
Vegard Nossum <vegard.nossum@gmail.com>
2009-05-27 08:41:32 +0200

    vm: get rid of obsolete function
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- include/vm/field.h
+++ include/vm/field.h
@@ -10,11 +10,6 @@
 #include <vm/vm.h>
 #include <vm/types.h>
 
-static inline enum vm_type field_type(struct fieldblock *fb)
-{
-	return str_to_type(fb->type);
-}
-
 struct vm_class;
 
 struct vm_field {
