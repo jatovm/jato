1d6f388b2eaa05646265474984540a471c6cb463
Tomek Grabiec <tgrabiec@gmail.com>
2009-05-29 11:49:14 +0200

    regression: tests for synchronized methods in jvm/SynchronizationTest.java
    Signed-off-by: Tomek Grabiec <tgrabiec@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- regression/jvm/SynchronizationTest.java
+++ regression/jvm/SynchronizationTest.java
@@ -36,8 +36,64 @@ public class SynchronizationTest extends TestCase {
         }
     }
 
+    public static synchronized int staticSynchronizedMethod(int x) {
+        return x;
+    }
+
+    public synchronized int synchronizedMethod(int x) {
+        return x;
+    }
+
+    public static synchronized void staticSynchronizedExceptingMethod() {
+        throw new RuntimeException();
+    }
+
+    public synchronized void synchronizedExceptingMethod() {
+        throw new RuntimeException();
+    }
+
+    public static void testSynchronizedExceptingMethod() {
+        SynchronizationTest test = new SynchronizationTest();
+        boolean caught = false;
+
+        try {
+            test.synchronizedExceptingMethod();
+        } catch (RuntimeException e) {
+            caught = true;
+        }
+
+        assertTrue(caught);
+    }
+
+    public static void testSynchronizedMethod() {
+        SynchronizationTest test = new SynchronizationTest();
+
+        assertEquals(3, test.synchronizedMethod(3));
+    }
+
+    public static void testStaticSynchronizedExceptingMethod() {
+        boolean caught = false;
+
+        try {
+            staticSynchronizedExceptingMethod();
+        } catch (RuntimeException e) {
+            caught = true;
+        }
+
+        assertTrue(caught);
+    }
+
+    public static void testStaticSynchronizedMethod() {
+        assertEquals(3, staticSynchronizedMethod(3));
+    }
+
+
     public static void main(String[] args) {
         testMonitorEnterAndExit();
+        testStaticSynchronizedMethod();
+        testSynchronizedMethod();
+        testStaticSynchronizedExceptingMethod();
+        testSynchronizedExceptingMethod();
 
         exit();
     }
