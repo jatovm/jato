cf3b32f57b00ee3a11a9a2fb2828786aea024170
Vegard Nossum <vegard.nossum@gmail.com>
2009-05-29 21:17:06 +0200

    test: add vm_class stubs
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

---
+++ test/vm/class-stub.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+#include <vm/class.h>
+#include <vm/field.h>
+#include <vm/method.h>
+
+struct vm_class *vm_class_resolve_class(struct vm_class *vmc, uint16_t i)
+{
+	NOT_IMPLEMENTED;
+	return NULL;
+}
+
+struct vm_field *vm_class_resolve_field_recursive(struct vm_class *vmc,
+	uint16_t i)
+{
+	NOT_IMPLEMENTED;
+	return NULL;
+}
+
+struct vm_method *vm_class_resolve_method_recursive(struct vm_class *vmc,
+	uint16_t i)
+{
+	NOT_IMPLEMENTED;
+	return NULL;
+}
