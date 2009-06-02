15ae7e9b187a1261ca8e81f02d2a6efa7416f75d
Tomek Grabiec <tgrabiec@gmail.com>
2009-06-02 15:24:26 +0200

    x86: select exception test after jato function calls.
    After every jato function call which may signal exception there should
    be exception test to check for it.
    
    Signed-off-by: Tomek Grabiec <tgrabiec@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

---
+++ arch/x86/thread.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2009 Tomasz Grabiec
+ *
+ * This file is released under the GPL version 2 with the following
+ * clarification and special exception:
+ *
+ *     Linking this library statically or dynamically with other modules is
+ *     making a combined work based on this library. Thus, the terms and
+ *     conditions of the GNU General Public License cover the whole
+ *     combination.
+ *
+ *     As a special exception, the copyright holders of this library give you
+ *     permission to link this library with independent modules to produce an
+ *     executable, regardless of the license terms of these independent
+ *     modules, and to copy and distribute the resulting executable under terms
+ *     of your choice, provided that you also meet, for each linked independent
+ *     module, the terms and conditions of the license of that module. An
+ *     independent module is a module which is not derived from or based on
+ *     this library. If you modify this library, you may extend this exception
+ *     to your version of the library, but you are not obligated to do so. If
+ *     you do not wish to do so, delete this exception statement from your
+ *     version.
+ *
+ * Please refer to the file LICENSE for details.
+ */
+
+#include <arch/thread.h>
+
+static unsigned long get_tls_address(void)
+{
+	unsigned long result;
+
+#ifdef CONFIG_X86_32
+	__asm__(
+		"movl %%gs:(0x0), %0 \n"
+		: "=r"(result) );
+#else
+ #error not implemented
+#endif
+
+	return result;
+}
+
+unsigned long get_thread_local_offset(void *thread_local_ptr)
+{
+#ifdef CONFIG_X86_32
+	return (unsigned long)thread_local_ptr - get_tls_address();
+#else
+ #error not implemented
+#endif
+}
