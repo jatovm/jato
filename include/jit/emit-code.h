c4a5759093fe5c0f2e38a6b61549efd95be91a96
Eduard - Gabriel Munteanu <eduard.munteanu@linux360.ro>
2009-06-16 11:50:20 +0300

    jit: add missing 'extern' keywords
    include/jit/emit-code.h declares prototypes without using 'extern'. This
    fixes the issue.
    
    Signed-off-by: Eduard - Gabriel Munteanu <eduard.munteanu@linux360.ro>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/jit/emit-code.h
+++ include/jit/emit-code.h
@@ -24,14 +24,14 @@ extern struct emitter emitters[];
 #define DECL_EMITTER(_insn_type, _fn, _emitter_type) \
 	[_insn_type] = { .emit_fn = _fn, .type = _emitter_type }
 
-void emit_prolog(struct buffer *, unsigned long);
-void emit_epilog(struct buffer *);
-void emit_trampoline(struct compilation_unit *, void *, struct jit_trampoline *);
-void emit_unwind(struct buffer *);
-void emit_lock(struct buffer *, struct object *);
-void emit_lock_this(struct buffer *);
-void emit_unlock(struct buffer *, struct object *);
-void emit_unlock_this(struct buffer *);
-void backpatch_branch_target(struct buffer *, struct insn *, unsigned long);
+extern void emit_prolog(struct buffer *, unsigned long);
+extern void emit_epilog(struct buffer *);
+extern void emit_trampoline(struct compilation_unit *, void *, struct jit_trampoline *);
+extern void emit_unwind(struct buffer *);
+extern void emit_lock(struct buffer *, struct object *);
+extern void emit_lock_this(struct buffer *);
+extern void emit_unlock(struct buffer *, struct object *);
+extern void emit_unlock_this(struct buffer *);
+extern void backpatch_branch_target(struct buffer *, struct insn *, unsigned long);
 
 #endif /* JATO_EMIT_CODE_H */
