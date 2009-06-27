99c5d0b5096bf9ae10386d8a1c2dadf7d111c5f8
Vegard Nossum <vegard.nossum@gmail.com>
2009-06-27 14:41:59 +0200

    runtime: provide VMSystem.arraycopy()
    We need this in order to actually find the method and notice that it's
    a native method.
    
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- runtime/classpath/java/lang/VMSystem.java
+++ runtime/classpath/java/lang/VMSystem.java
@@ -1,5 +1,6 @@
 package java.lang;
 
 public class VMSystem {
+    static native void arraycopy(Object src, int srcStart, Object dest, int destStart, int len);
     static native int identityHashCode(Object o);
 }
