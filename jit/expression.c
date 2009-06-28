63225a2d70f0ad981a110c28ee474c9161643478
Pekka Enberg <penberg@cs.helsinki.fi>
2009-06-28 17:57:10 +0300

    jit: Remove unused function declaration from jit/expression.c
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- jit/expression.c
+++ jit/expression.c
@@ -147,8 +147,6 @@ void free_expression(struct expression *expr)
 	free(expr);
 }
 
-extern void print_trace(void);
-
 struct expression *expr_get(struct expression *expr)
 {
 	assert(expr->refcount > 0);
