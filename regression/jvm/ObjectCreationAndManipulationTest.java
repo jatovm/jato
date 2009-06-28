f8e69e3b797f33846690c36c278a9b1466b0faac
Vegard Nossum <vegard.nossum@gmail.com>
2009-06-28 13:15:55 +0200

    regression: re-enable ObjectCreationAndManipulationTest
    This works now, so let's reenable it.
    
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- regression/jvm/ObjectCreationAndManipulationTest.java
+++ regression/jvm/ObjectCreationAndManipulationTest.java
@@ -159,14 +159,12 @@ public class ObjectCreationAndManipulationTest extends TestCase {
         testArrayLength();
         testMultiANewArray();
         testIsInstanceOf();
-/*	FIXME:
         testIntArrayLoadAndStore();
         testCharArrayLoadAndStore();
         testByteArrayLoadAndStore();
         testShortArrayLoadAndStore();
         testObjectArrayLoadAndStore();
         testMultiDimensionalArrayLoadAndStore();
-*/
         testCheckCast();
 
         exit();
