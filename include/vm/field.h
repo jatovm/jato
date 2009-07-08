9e44d2edbc6656983acf755642869b5eb4222872
Vegard Nossum <vegard.nossum@gmail.com>
2009-07-09 01:22:36 +0200

    vm: constify field flag helper arguments
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/vm/field.h
+++ include/vm/field.h
@@ -28,17 +28,17 @@ int vm_field_init(struct vm_field *vmf,
 void vm_field_init_nonstatic(struct vm_field *vmf, unsigned int offset);
 int vm_field_init_static(struct vm_field *vmf, unsigned int offset);
 
-static inline bool vm_field_is_static(struct vm_field *vmf)
+static inline bool vm_field_is_static(const struct vm_field *vmf)
 {
 	return vmf->field->access_flags & CAFEBABE_FIELD_ACC_STATIC;
 }
 
-static inline bool vm_field_is_final(struct vm_field *vmf)
+static inline bool vm_field_is_final(const struct vm_field *vmf)
 {
 	return vmf->field->access_flags & CAFEBABE_FIELD_ACC_FINAL;
 }
 
-static inline enum vm_type vm_field_type(struct vm_field *vmf)
+static inline enum vm_type vm_field_type(const struct vm_field *vmf)
 {
 	return str_to_type(vmf->type);
 }
