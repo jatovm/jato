b1191bcd98391882e9c01a4b596351212c929f9c
Tomek Grabiec <tgrabiec@gmail.com>
2009-07-12 23:10:33 +0200

    vm: constify argument of bytecode read functions
    Acked-by: Vegard Nossum <vegard.nossum@gmail.com>
    Signed-off-by: Tomek Grabiec <tgrabiec@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/vm/bytecode.h
+++ include/vm/bytecode.h
@@ -16,10 +16,10 @@ int32_t bytecode_read_s32(struct bytecode_buffer *buffer);
 uint32_t bytecode_read_u32(struct bytecode_buffer *buffer);
 int32_t bytecode_read_branch_target(unsigned char opc, struct bytecode_buffer *buffer);
 
-uint8_t read_u8(unsigned char *p);
-int16_t read_s16(unsigned char *p);
-uint16_t read_u16(unsigned char *p);
-int32_t read_s32(unsigned char *p);
-uint32_t read_u32(unsigned char *p);
+uint8_t read_u8(const unsigned char *p);
+int16_t read_s16(const unsigned char *p);
+uint16_t read_u16(const unsigned char *p);
+int32_t read_s32(const unsigned char *p);
+uint32_t read_u32(const unsigned char *p);
 
 #endif
