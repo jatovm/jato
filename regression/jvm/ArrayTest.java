8687c5e084999ca1dea483536150316826576460
Pekka Enberg <penberg@cs.helsinki.fi>
2009-07-21 13:23:00 +0300

    regression: Remove test case that was committed by mistake
    Commit e7bd57fa4f744207b5e1c3098baa7af63282addb ("x86: Long arithmetic
    shift operations") added an array clone test case by accident.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- regression/jvm/ArrayTest.java
+++ regression/jvm/ArrayTest.java
@@ -139,15 +139,6 @@ public class ArrayTest extends TestCase {
         assertTrue(arr2.getClass().equals(big_arr[0][0].getClass()));
     }
 
-    public static void testArrayClone() {
-//        int a[] = new int[10];
-        int a[] = null;
-        int b[] = null;
-
-        b = (a != null ? (int[]) a.clone() : new int[0]);
-        assertNotNull(b);
-    }
-
     public static void main(String args[]) {
         testEmptyStaticArrayLength();
         testEmptyArrayLength();
@@ -164,6 +155,5 @@ public class ArrayTest extends TestCase {
         testReferenceElementLoadStore();
 
         testArrayClass();
-        testArrayClone();
     }
 }
