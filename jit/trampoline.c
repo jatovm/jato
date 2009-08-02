8e32ab1890ffb88a8f70e27f4a2a458d68234154
Tomek Grabiec <tgrabiec@gmail.com>
2009-08-03 00:10:00 +0200

    jit: move fixup_static() call for static method invocations after compilation unit is unlocked
    fixup_static() locks on appropriate compilation units so we must not
    call it with cu mutex lock.
    
    Signed-off-by: Tomek Grabiec <tgrabiec@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- jit/trampoline.c
+++ jit/trampoline.c
@@ -139,14 +139,15 @@ void *jit_magic_trampoline(struct compilation_unit *cu)
 	 * Therefore, do fixup for direct call sites unconditionally and fixup
 	 * vtables if method can be invoked via invokevirtual.
 	 */
-	if (ret) {
+	if (ret)
 		fixup_direct_calls(method->trampoline, (unsigned long) ret);
 
-		if (vm_method_is_static(cu->method))
-			fixup_static(cu->method->class);
-	}
-
 	pthread_mutex_unlock(&cu->mutex);
+
+	/* XXX: this must be done with cu->mutex unlocked because fixup_static()
+	 * might need to lock it. */
+	if (ret && vm_method_is_static(cu->method))
+		fixup_static(cu->method->class);
 
 	return ret;
 }
