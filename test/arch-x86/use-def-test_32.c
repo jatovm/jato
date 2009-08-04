85b743f0dd96f0ff04f92055f0498acd04d819bb
Arthur Huillet <arthur.huillet@free.fr>
2009-08-04 13:46:47 +0200

    x86: add USE_DST to MUL
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- test/arch-x86/use-def-test_32.c
+++ test/arch-x86/use-def-test_32.c
@@ -146,7 +146,7 @@ void test_memlocal_reg_defines_target(void)
 void test_membase_reg_uses_source_defines_target(void)
 {
 	assert_uses_r0_r1_defines_r1(membase_reg_insn(INSN_ADD_MEMBASE_REG, &r0, 0, &r1));
-	assert_uses_r0_defines_r1(membase_reg_insn(INSN_AND_MEMBASE_REG, &r0, 0, &r1));
+	assert_uses_r0_r1_defines_r1(membase_reg_insn(INSN_AND_MEMBASE_REG, &r0, 0, &r1));
 	assert_uses_r0_defines_r1(membase_reg_insn(INSN_CMP_MEMBASE_REG, &r0, 0, &r1));
 	assert_uses_r0_r1_defines_r1(membase_reg_insn(INSN_DIV_MEMBASE_REG, &r0, 0, &r1));
 	assert_uses_r0_defines_r1(membase_reg_insn(INSN_MOV_MEMBASE_REG, &r0, 0, &r1));
