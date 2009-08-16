e0519dd481e07c50f15987f3a9e166220a99cf73
Tomek Grabiec <tgrabiec@gmail.com>
2009-08-16 22:52:13 +0200

    vm: implement properly VMStackWalker.getClassContext()
    Signed-off-by: Tomek Grabiec <tgrabiec@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- vm/jato.c
+++ vm/jato.c
@@ -100,24 +100,56 @@ static struct vm_object *native_vmstackwalker_getclasscontext(void)
 {
 	struct stack_trace_elem st_elem;
 	struct compilation_unit *cu;
-	struct vm_class *class;
 	struct vm_object *res;
+	int nr_to_skip;
+	int depth;
+	int idx;
+	int i;
 
 	init_stack_trace_elem_current(&st_elem);
+	depth = get_java_stack_trace_depth(&st_elem);
 
-	if (stack_trace_elem_next_java(&st_elem))
-		return NULL;
+	/* Skip call to VMStackWalker.getClassContext() */
+	depth -= 2;
+	stack_trace_elem_next_java(&st_elem);
+	stack_trace_elem_next_java(&st_elem);
 
-	cu = jit_lookup_cu(st_elem.addr);
-	if (!cu) {
-		NOT_IMPLEMENTED;
+	res = vm_object_alloc_array(vm_array_of_java_lang_Class, depth);
+	if (!res) {
+		signal_new_exception(vm_java_lang_OutOfMemoryError, NULL);
 		return NULL;
 	}
 
-	class = cu->method->class;
+	nr_to_skip = 0;
+	idx = 0;
 
-	res = vm_object_alloc_array(vm_java_lang_Class, 1);
-	array_set_field_object(res, 0, class->object);
+	for (i = 0; i < depth; i++, stack_trace_elem_next_java(&st_elem)) {
+		cu = jit_lookup_cu(st_elem.addr);
+		if (!cu)
+			error("no compilation_unit mapping for %lx", st_elem.addr);
+
+		struct vm_class *vmc = cu->method->class;
+
+		if (vmc == vm_java_lang_reflect_Method &&
+		    !strcmp(cu->method->name, "invoke")) {
+			/*
+			 * We should skip all Method.invoke() frames
+			 * here as stated in comment for
+			 * VMStackWalker.getClassContext().
+			 */
+			nr_to_skip++;
+			continue;
+		}
+
+		if (vm_class_ensure_init(vmc))
+			return NULL;
+
+		printf("%d: %s.%s\n", idx, vmc->name, cu->method->name);
+
+		array_set_field_object(res, idx++, vmc->object);
+	}
+
+	res->array_length -= nr_to_skip;
 
 	return res;
 }
