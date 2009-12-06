4f0d5bac6ca325bbbbb009cac3b2710ae937073a
Pekka Enberg <penberg@cs.helsinki.fi>
2009-12-06 13:57:50 +0200

    jit: Fix GC_REGISTER_MAP_SIZE to use BITS_PER_LONG
    As explained by Tomek, use BITS_PER_LONG to calculate the number of unsigned
    longs we need to hold a bitmap NR_GP_REGISTERS.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/jit/gc-map.h
+++ include/jit/gc-map.h
@@ -4,7 +4,7 @@
 #include <arch/registers.h>
 #include <vm/system.h>
 
-#define GC_REGISTER_MAP_SIZE	DIV_ROUND_UP(NR_GP_REGISTERS, sizeof(unsigned long))
+#define GC_REGISTER_MAP_SIZE	DIV_ROUND_UP(NR_GP_REGISTERS, BITS_PER_LONG)
 
 struct gc_map {
 	unsigned long		register_map[GC_REGISTER_MAP_SIZE];	/* references in registers */
