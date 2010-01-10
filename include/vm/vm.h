32379e01840ddc3679eb390678fc2f48ba4f58b6
Pekka Enberg <penberg@cs.helsinki.fi>
2010-01-10 15:30:51 +0200

    vm: Remove JamVM compat macros
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -16,13 +16,4 @@ extern char *slash2dots(char *utf8);
 #define T_INT                  10
 #define T_LONG                 11
 
-/* XXX: Users of these definitions are likely to be buggy.  */
-
-#define ARRAY_LEN(arrayRef)             *(uint32_t*)(arrayRef+1)
-#define ARRAY_DATA(arrayRef)            ((void*)(((uint32_t*)(arrayRef+1))+1))
-#define INST_DATA(objectRef)            ((uintptr_t*)(objectRef+1))
-
-#define CLASS_ARRAY             6
-#define IS_ARRAY(cb)                    (cb->state == CLASS_ARRAY)
-
 #endif /* JATO_VM_H */
