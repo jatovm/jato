45c6ed7118530ef674b83bca11f278adc2700edb
Eduard - Gabriel Munteanu <eduard.munteanu@linux360.ro>
2010-04-30 20:58:56 +0300

    x86-64: remove obsolete fields from stack frame
    The stack frame no longer saves callee-saved registers before pushing
    %rbp.
    
    Signed-off-by: Eduard - Gabriel Munteanu <eduard.munteanu@linux360.ro>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- arch/x86/include/arch/stack-frame.h
+++ arch/x86/include/arch/stack-frame.h
@@ -31,27 +31,8 @@ struct jit_stack_frame {
 
 #define NR_TRAMPOLINE_LOCALS	14
 
-/*
- * FIXME: This no longer reflects the structure
- * of the stack frame and should be fixed, although
- * it provides the correct answer in most cases. It is a bug!
- */
 struct jit_stack_frame {
 	void *prev; /* previous stack frame link */
-	unsigned long old_xmm15;
-	unsigned long old_xmm14;
-	unsigned long old_xmm13;
-	unsigned long old_xmm12;
-	unsigned long old_xmm11;
-	unsigned long old_xmm10;
-	unsigned long old_xmm9;
-	unsigned long old_xmm8;
-	unsigned long old_r15;
-	unsigned long old_r14;
-	unsigned long old_r13;
-	unsigned long old_r12;
-	unsigned long old_rbx;
-	unsigned long this;
 	unsigned long return_address;
 	unsigned long args[0];
 } __attribute__((packed));
