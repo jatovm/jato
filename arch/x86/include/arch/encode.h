11ba78d7674dce178ebed890f4a895e94a86a495
Pekka Enberg <penberg@cs.helsinki.fi>
2010-07-18 22:33:07 +0300

    x86: Extract fixup code in a separate file
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- arch/x86/include/arch/encode.h
+++ arch/x86/include/arch/encode.h
@@ -9,6 +9,14 @@ struct basic_block;
 struct buffer;
 struct insn;
 
+#define X86_CALL_INSN_SIZE	5
+
+#define	REX		0x40
+#define REX_W		(REX | 8)	/* 64-bit operands */
+#define REX_R		(REX | 4)	/* ModRM reg extension */
+#define REX_X		(REX | 2)	/* SIB index extension */
+#define REX_B		(REX | 1)	/* ModRM r/m extension */
+
 uint8_t x86_encode_reg(enum machine_reg reg);
 
 static inline uint8_t x86_encode_mod_rm(uint8_t mod, uint8_t reg_opcode, uint8_t rm)
