a46548d2a5e5da8d0cbe5c0f7b492e1b3af2ed7d
Pekka Enberg <penberg@kernel.org>
2011-05-01 10:12:01 +0300

    vm: Lindent vm/debug.c
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- vm/debug.c
+++ vm/debug.c
@@ -8,12 +8,11 @@ void debug_print_vm_method(struct vm_method *method)
 {
 	if (method) {
 		printf("  method : %s %s(", method->type, method->name);
-		for (int i = 0; i < method->args_count; i++ ) {
+		for (int i = 0; i < method->args_count; i++) {
 			printf(".");
 		}
 		printf(")\n");
-	}
-	else {
+	} else {
 		printf("Unable to print null vm_method!\n");
 	}
 }
@@ -22,18 +21,21 @@ void debug_print_vm_class(struct vm_class *class)
 {
 	if (class) {
 		printf("vm_class address : %p\n", class);
-		if (class->name) printf("class->name : %s\n", class->name);
-		if (class->super) printf("class->super->name : %s\n", class->super->name);
+		if (class->name)
+			printf("class->name : %s\n", class->name);
+		if (class->super)
+			printf("class->super->name : %s\n", class->super->name);
 		printf("class->kind : %d\n", class->kind);
 		printf("class->state : %d\n", class->state);
-		if (class->declaring_class) printf("class->declaring_class->name : %s\n", class->declaring_class->name);
-		if (class->enclosing_class) printf("class->enclosing_class->name : %s\n", class->enclosing_class->name);
+		if (class->declaring_class)
+			printf("class->declaring_class->name : %s\n", class->declaring_class->name);
+		if (class->enclosing_class)
+			printf("class->enclosing_class->name : %s\n", class->enclosing_class->name);
 
-		for(uint i = 0; i < class->nr_methods; i++) {
+		for (uint i = 0; i < class->nr_methods; i++) {
 			debug_print_vm_method(&class->methods[i]);
 		}
-	}
-	else {
+	} else {
 		printf("Unable to print null vm_class!\n");
 	}
 }
@@ -43,8 +45,7 @@ void debug_print_vm_object(struct vm_object *obj)
 	if (obj) {
 		printf("vm_object address : %p\n", obj);
 		debug_print_vm_class(obj->class);
-	}
-	else {
+	} else {
 		printf("Unable to print null vm_object!\n");
 	}
 }
