9f9d60d1254b1dd437bfb50dd3c460c309f590b8
Pekka Enberg <penberg@kernel.org>
2011-05-03 18:58:08 +0300

    x86-64: Fix EXPR_VALUE code generation for 64-bit immediates
    Cc: Eduard - Gabriel Munteanu <eduard.munteanu@linux360.ro>
    Cc: Sergey Mashkov <cy6erGn0m@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- tools/test.py
+++ tools/test.py
@@ -86,7 +86,7 @@ TESTS = [
 , ( "jvm.WideTest", 0, NO_SYSTEM_CLASSLOADER, [ "i386", "x86_64" ] )
 , ( "jvm.lang.reflect.FieldTest", 0, NO_SYSTEM_CLASSLOADER, [ "i386" ] )
 , ( "jvm.lang.ReferenceTest", 0, NO_SYSTEM_CLASSLOADER, [ "i386", "x86_64" ] )
-, ( "sun.misc.UnsafeTest", 0, NO_SYSTEM_CLASSLOADER, [ "i386" ] )
+, ( "sun.misc.UnsafeTest", 0, NO_SYSTEM_CLASSLOADER, [ "i386", "x86_64" ] )
 , ( "test.java.lang.ClassTest", 0, [ ], [ "i386" ] )
 , ( "test.java.lang.DoubleTest", 0, [ ], [ "i386" ] )
 , ( "test.java.lang.reflect.MethodTest", 0, [ ], [ "i386" ] )
