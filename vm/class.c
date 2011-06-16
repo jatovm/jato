8821e1b907cf5b7c32100c6bdde94a87e03c493b
Pekka Enberg <penberg@kernel.org>
2011-06-16 20:56:07 +0300

    vm: Make subtype checking faster
    This patch adds a supertype cache to 'struct vm_class' for speeding up subtype
    checks (e.g. instanceof). This patch speeds up DaCapo bloat benchmark by 3.9%.
    
    Before:
    
    ===== DaCapo bloat PASSED in 58023 msec =====
    
     Performance counter stats for 'jato -Xperf -jar dacapo-2006-10-MR2.jar bloat':
    
            41,723,840 cache-misses             #      0.726 M/sec  (scaled from 33.38%)
           801,762,869 cache-references         #     13.955 M/sec  (scaled from 33.44%)
           302,131,921 branch-misses            #      1.342 %      (scaled from 33.32%)
        22,511,533,496 branches                 #    391.818 M/sec  (scaled from 33.29%)
       131,983,320,018 instructions             #      1.160 IPC    (scaled from 33.31%)
       113,748,889,761 cycles                   #   1979.824 M/sec  (scaled from 33.28%)
                28,933 page-faults              #      0.001 M/sec
                     0 CPU-migrations           #      0.000 M/sec
                10,622 context-switches         #      0.000 M/sec
          57454.044543 task-clock-msecs         #      0.962 CPUs
    
           59.725130659  seconds time elapsed
    
    After:
    
    ===== DaCapo bloat PASSED in 56118 msec =====
    
     Performance counter stats for 'jato -Xperf -jar dacapo-2006-10-MR2.jar bloat':
    
            42,395,725 cache-misses             #      0.784 M/sec  (scaled from 33.37%)
           799,537,938 cache-references         #     14.793 M/sec  (scaled from 33.37%)
           309,008,372 branch-misses            #      1.480 %      (scaled from 33.26%)
        20,877,849,165 branches                 #    386.285 M/sec  (scaled from 33.30%)
       121,237,876,431 instructions             #      1.127 IPC    (scaled from 33.38%)
       107,553,098,499 cycles                   #   1989.966 M/sec  (scaled from 33.34%)
                28,984 page-faults              #      0.001 M/sec
                     0 CPU-migrations           #      0.000 M/sec
                10,770 context-switches         #      0.000 M/sec
          54047.716825 task-clock-msecs         #      0.942 CPUs
    
           57.364907273  seconds time elapsed
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- vm/class.c
+++ vm/class.c
@@ -1232,6 +1232,13 @@ bool vm_class_is_anonymous(const struct vm_class *vmc)
 	return is_numeric(separator + 1);
 }
 
+static void supertype_cache_update(struct vm_class *vmc, const struct vm_class *super)
+{
+	unsigned int ndx = vmc->supertype_cache_ndx++ % SUPERTYPE_CACHE_SIZE;
+
+	vmc->supertype_cache[ndx]	= super;
+}
+
 static bool vm_class_is_subclass_of(const struct vm_class *vmc, const struct vm_class *from)
 {
 	struct vm_class *super = from->super;
@@ -1246,23 +1253,26 @@ static bool vm_class_is_subclass_of(const struct vm_class *vmc, const struct vm_
 	return false;
 }
 
+static bool vm_class_is_assignable_from_nocache(const struct vm_class *vmc, const struct vm_class *from);
+
 static bool vm_class_is_instance_of_array(const struct vm_class *vmc, const struct vm_class *from)
 {
 	if (!vm_class_is_array_class(from))
 		return false;
 
-	const struct vm_class *vmc_el = vm_class_get_array_element_class(vmc);
+	struct vm_class *vmc_el = vm_class_get_array_element_class(vmc);
 
-	const struct vm_class *from_el = vm_class_get_array_element_class(vmc);
+	struct vm_class *from_el = vm_class_get_array_element_class(vmc);
 
-	return vm_class_is_assignable_from(vmc_el, from_el);
+	return vm_class_is_assignable_from_nocache(vmc_el, from_el);
 }
 
 static bool vm_class_implements(const struct vm_class *vmc, const struct vm_class *from)
 {
 	for (unsigned int i = 0; i < from->nr_interfaces; ++i) {
-		if (vmc == from->interfaces[i] || vm_class_implements(vmc, from->interfaces[i]))
+		if (vmc == from->interfaces[i] || vm_class_implements(vmc, from->interfaces[i])) {
 			return true;
+		}
 	}
 
 	if (from->super)
@@ -1271,8 +1281,7 @@ static bool vm_class_implements(const struct vm_class *vmc, const struct vm_clas
 	return false;
 }
 
-/* Reference: http://download.oracle.com/javase/1.5.0/docs/api/java/lang/Class.html#isAssignableFrom(java.lang.Class) */
-bool vm_class_is_assignable_from(const struct vm_class *vmc, const struct vm_class *from)
+static bool vm_class_is_assignable_from_nocache(const struct vm_class *vmc, const struct vm_class *from)
 {
 	if (vmc == from)
 		return true;
@@ -1284,6 +1293,19 @@ bool vm_class_is_assignable_from(const struct vm_class *vmc, const struct vm_cla
 		return vm_class_is_instance_of_array(vmc, from);
 
 	return vm_class_is_subclass_of(vmc, from);
+}
+
+/* Reference: http://download.oracle.com/javase/1.5.0/docs/api/java/lang/Class.html#isAssignableFrom(java.lang.Class) */
+bool vm_class_is_assignable_from_slow(struct vm_class *vmc, const struct vm_class *from)
+{
+	bool ret;
+
+	ret = vm_class_is_assignable_from_nocache(vmc, from);
+
+	if (ret)
+		supertype_cache_update(vmc, from);
+
+	return ret;
 }
 
 char *vm_class_get_array_element_class_name(const char *class_name)
