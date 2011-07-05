7459c5aac4b5ae6f9b9dcb40393755dd74490eab
Ana Farcasi <farcasia@gmail.com>
2011-07-05 17:23:33 +0300

    test/unit: Add new test for tableswitch
    This patch adds a new test for the tableswitch instruction.
    
    Signed-off-by: Ana Farcasi <farcasia@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- test/unit/jit/switch-bc-test.c
+++ test/unit/jit/switch-bc-test.c
@@ -8,7 +8,7 @@
 #include <libharness.h>
 #include <basic-block-assert.h>
 
-/* Test for tableswitch:
+/* Test1 for tableswitch:
 	int tableswitch(int i) {
 		switch (i) {
 			case 0:  return 0;
@@ -18,7 +18,7 @@
 		}
 	}
  */
-static unsigned char tableswitch[36] = {
+static unsigned char tableswitch1[36] = {
 	0x1b, 0xaa, 0x00, 0x00, 0x00, 0x00, 0x00, 0x21,
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
 	0x00, 0x00, 0x00, 0x1b, 0x00, 0x00, 0x00, 0x1d,
@@ -26,15 +26,15 @@ static unsigned char tableswitch[36] = {
 	0x05, 0xac, 0x02, 0xac,
 };
 
-void test_convert_tableswitch_bbs()
+void test1_convert_tableswitch_bbs(void)
 {
 	struct basic_block *bb0, *bb1, *bb2, *bb3, *bb4, *bb5, *bb6;
 	struct compilation_unit *cu;
 
 	struct cafebabe_method_info method_info;
 	struct vm_method method = {
-		.code_attribute.code = tableswitch,
-		.code_attribute.code_length = ARRAY_SIZE(tableswitch),
+		.code_attribute.code = tableswitch1,
+		.code_attribute.code_length = ARRAY_SIZE(tableswitch1),
 		.method = &method_info,
 	};
 
@@ -59,6 +59,61 @@ void test_convert_tableswitch_bbs()
 	assert_basic_block_successors((struct basic_block*[]){ }, 0, bb4);
 	assert_basic_block_successors((struct basic_block*[]){ }, 0, bb5);
 	assert_basic_block_successors((struct basic_block*[]){ }, 0, bb6);
+}
+
+/* Test2 for tableswitch:
+	void tableswitch() {
+		int a = 1;
+		switch (a) {
+			case 0:
+				a = 10;
+			case 1:
+				a = 30;
+			case 5:
+				a = 50;
+		}
+	}
+ */
+static unsigned char tableswitch2[46] = {
+	0x04, 0x3c, 0x1b, 0xab, 0x00, 0x00, 0x00, 0x2a,
+	0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01,
+	0x00, 0x00, 0x00, 0x24, 0x00, 0x00, 0x00, 0x05,
+	0x00, 0x00, 0x00, 0x27, 0x10, 0x0a, 0x3c, 0x10,
+	0x1e, 0x3c, 0x10, 0x28, 0x3c, 0xb1,
+};
+
+void test2_convert_tableswitch_bbs(void)
+{
+	struct basic_block *bb0, *bb1, *bb2, *bb3, *bb4, *bb5;
+	struct compilation_unit *cu;
+
+	struct cafebabe_method_info method_info;
+	struct vm_method method = {
+		.code_attribute.code = tableswitch2,
+		.code_attribute.code_length = ARRAY_SIZE(tableswitch2),
+		.method = &method_info,
+	};
+
+	cu = compilation_unit_alloc(&method);
+
+	analyze_control_flow(cu);
+	convert_to_ir(cu);
+	assert_int_equals(6, nr_bblocks(cu));
+
+	bb0 = bb_entry(cu->bb_list.next);
+	bb1 = bb_entry(bb0->bb_list_node.next);
+	bb2 = bb_entry(bb1->bb_list_node.next);
+	bb3 = bb_entry(bb2->bb_list_node.next);
+	bb4 = bb_entry(bb3->bb_list_node.next);
+	bb5 = bb_entry(bb4->bb_list_node.next);
+
+	assert_basic_block_successors((struct basic_block*[]){bb5, bb1}, 2, bb0);
+	assert_basic_block_successors((struct basic_block*[]){bb2, bb3, bb4, bb0}, 4, bb1);
+	assert_basic_block_successors((struct basic_block*[]){bb3}, 1, bb2);
+	assert_basic_block_successors((struct basic_block*[]){bb4}, 1, bb3);
+	assert_basic_block_successors((struct basic_block*[]){bb5}, 1, bb4);
+	assert_basic_block_successors((struct basic_block*[]){ }, 0, bb5);
 }
 
 /* Test for lookupswitch:
