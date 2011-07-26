711cefa10430b014efd4c4641e1f301d3d4b3e85
Pekka Enberg <penberg@kernel.org>
2011-07-27 00:31:00 +0300

    ppc, linux: Add stubs for backtrace.c
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ sys/linux-ppc/backtrace.c
@@ -0,0 +1,13 @@
+#include "vm/backtrace.h"
+
+#include <assert.h>
+
+void print_backtrace_and_die(int a, siginfo_t *st, void *a1)
+{
+	assert(!"not implemented");
+}
+
+void show_function(void *addr)
+{
+	assert(!"not implemented");
+}
