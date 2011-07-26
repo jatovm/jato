3a13210aa23a66c2504699e88e47f4e86d9d9778
Pekka Enberg <penberg@kernel.org>
2011-07-27 00:49:18 +0300

    arm: Kill unnecessary 'extern' modifier
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- sys/linux-arm/backtrace.c
+++ sys/linux-arm/backtrace.c
@@ -6,7 +6,8 @@ void print_backtrace_and_die(int a, siginfo_t *st, void *a1)
 {
 	assert(!"not implemented");
 }
-extern void show_function(void *addr)
+
+void show_function(void *addr)
 {
 	assert(!"not implemented");
 }
