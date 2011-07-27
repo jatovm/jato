0c7018b84ef02c9185bc256909e59eb95cf7efd3
Pekka Enberg <penberg@kernel.org>
2011-07-27 15:44:54 +0300

    ppc: Fix up struct jit_stack_frame
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- arch/ppc/include/arch/stack-frame.h
+++ arch/ppc/include/arch/stack-frame.h
@@ -7,7 +7,9 @@ struct native_stack_frame {
 } __attribute__((packed));
 
 struct jit_stack_frame {
-	unsigned long	return_address;
+	void			*prev;		/* previous stack frame link */
+	unsigned long 		return_address;
+	unsigned long		args[0];
 };
 
 static inline unsigned long frame_locals_size(struct stack_frame *frame)
