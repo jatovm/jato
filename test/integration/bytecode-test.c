09f40f820817abb537c1cfb532d649bab7cb3167
Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
2011-08-30 19:05:04 +0800

    test/integration: Added some more bytecode tests.
    Signed-off-by: Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- test/integration/bytecode-test.c
+++ test/integration/bytecode-test.c
@@ -59,7 +59,7 @@ static jint do_execute(uint8_t *code, unsigned long code_length)
 		.name		= "foo",
 		.class		= &vmc,
 		.code_attribute = (struct cafebabe_code_attribute) {
-			.max_locals	= 2,
+			.max_locals	= 4,
 			.code		= code,
 			.code_length	= code_length,
 		},
@@ -101,6 +101,7 @@ static jdouble do_jdouble_execute(uint8_t *code, unsigned long code_length)
 		.name		= "foo",
 		.class		= &vmc,
 		.code_attribute = (struct cafebabe_code_attribute) {
+			.max_locals	= 2,
 			.code		= code,
 			.code_length	= code_length,
 		},
@@ -142,6 +143,7 @@ static jfloat do_jfloat_execute(uint8_t *code, unsigned long code_length)
 		.name		= "foo",
 		.class		= &vmc,
 		.code_attribute = (struct cafebabe_code_attribute) {
+			.max_locals	= 2,
 			.code		= code,
 			.code_length	= code_length,
 		},
@@ -183,6 +185,7 @@ static jobject do_jobject_execute(uint8_t *code, unsigned long code_length)
 		.name		= "foo",
 		.class		= &vmc,
 		.code_attribute = (struct cafebabe_code_attribute) {
+			.max_locals	= 2,
 			.code		= code,
 			.code_length	= code_length,
 		},
@@ -954,6 +957,48 @@ static void test_istore_1(void)
 	assert_int_equals(1, execute(bytecode));
 }
 
+static void test_lstore_0(void)
+{
+	uint8_t bytecode[] = { OPC_LCONST_1, OPC_LSTORE_0, OPC_LLOAD_0, OPC_LRETURN};
+
+	assert_long_equals(1, jlong_run(bytecode));
+}
+
+static void test_lstore_1(void)
+{
+	uint8_t bytecode[] = { OPC_LCONST_1, OPC_LSTORE_1, OPC_LLOAD_1, OPC_LRETURN};
+
+	assert_long_equals(1, jlong_run(bytecode));
+}
+
+static void test_fstore_0(void)
+{
+	uint8_t bytecode[] = { OPC_FCONST_1, OPC_FSTORE_0, OPC_FLOAD_0, OPC_FRETURN};
+
+	assert_float_equals(1, jfloat_run(bytecode));
+}
+
+static void test_fstore_1(void)
+{
+	uint8_t bytecode[] = { OPC_FCONST_1, OPC_FSTORE_1, OPC_FLOAD_1, OPC_FRETURN};
+
+	assert_float_equals(1, jfloat_run(bytecode));
+}
+
+static void test_dstore_0(void)
+{
+	uint8_t bytecode[] = { OPC_DCONST_1, OPC_DSTORE_0, OPC_DLOAD_0, OPC_DRETURN};
+
+	assert_double_equals(1, jdouble_run(bytecode));
+}
+
+static void test_dstore_1(void)
+{
+	uint8_t bytecode[] = { OPC_DCONST_1, OPC_DSTORE_1, OPC_DLOAD_1, OPC_DRETURN};
+
+	assert_double_equals(1, jdouble_run(bytecode));
+}
+
 #ifndef CONFIG_ARM
 
 static void test_i2f(void)
@@ -1129,19 +1174,19 @@ static void run_tests(void)
 #endif
 	test_istore_0();
 	test_istore_1();
+	test_lstore_0();
+	test_lstore_1();
+	test_fstore_0();
+	test_fstore_1();
+	test_dstore_0();
+	test_dstore_1();
 #ifndef CONFIG_ARM
 	/* test_istore_2(); */
 	/* test_istore_3(); */
-	/* test_lstore_0(); */
-	/* test_lstore_1(); */
 	/* test_lstore_2(); */
 	/* test_lstore_3(); */
-	/* test_fstore_0(); */
-	/* test_fstore_1(); */
 	/* test_fstore_2(); */
 	/* test_fstore_3(); */
-	/* test_dstore_0(); */
-	/* test_dstore_1(); */
 	/* test_dstore_2(); */
 	/* test_dstore_3(); */
 	/* test_astore_0(); */
