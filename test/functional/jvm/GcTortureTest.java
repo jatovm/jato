5a02ef7955ffc663977ba7c9ab445b76f2a2f646
Pekka Enberg <penberg@kernel.org>
2011-09-08 17:16:21 +0300

    test, functional: Improve GC torture test
    This patch improves GC torture test by adding one thread that does no GC at all
    to make sure there's at least one thread that's suspended in JIT'd code.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- test/functional/jvm/GcTortureTest.java
+++ test/functional/jvm/GcTortureTest.java
@@ -4,11 +4,21 @@ public class GcTortureTest {
     public static void main(String[] args) throws Exception {
         Thread[] threads = new Thread[32];
 
-        for (int i = 0; i < threads.length; i++) {
+        /* One thread doesn't do any GC.  */
+        threads[0] = new Thread(new Runnable() {
+            int result;
+
+            public void run() {
+                for (int i = 0; i < 100000; i++)
+                    result += i;
+            }
+        });
+
+        for (int i = 1; i < threads.length; i++) {
             threads[i] = new Thread(new Runnable() {
                 public void run() {
                     for (int i = 0; i < 10000; i++)
-                      new Object();
+                        new Object();
                 }
             });
         }
