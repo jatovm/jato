e02213005e1410f1e7115a504d229bc0e346a079
Pekka Enberg <penberg@kernel.org>
2012-02-02 19:43:38 +0200

    arm, linux: Implement signal handling functions
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- sys/linux-arm/signal.c
+++ sys/linux-arm/signal.c
@@ -6,7 +6,18 @@
 
 bool signal_from_native(void *ctx)
 {
-	assert(!"not implemented");
+	ucontext_t *uc = ctx;
+	struct sigcontext *sc = &uc->uc_mcontext;
+
+	return is_native(sc->arm_pc);
+}
+
+struct compilation_unit *get_signal_source_cu(void *ctx)
+{
+	ucontext_t *uc = ctx;
+	struct sigcontext *sc = &uc->uc_mcontext;
+
+	return jit_lookup_cu(sc->arm_pc);
 }
 
 int install_signal_bh(void *ctx, signal_bh_fn bh)
@@ -14,7 +25,3 @@ int install_signal_bh(void *ctx, signal_bh_fn bh)
 	assert(!"not implemented");
 }
 
-struct compilation_unit *get_signal_source_cu(void *ctx)
-{
-	assert(!"not implemented");
-}
