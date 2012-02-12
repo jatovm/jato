5da28899d3ec92b8b26c83a200d4478751577996
Pekka Enberg <penberg@kernel.org>
2012-02-12 12:55:20 +0200

    arm: Add missing arg_offset() function prototype
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- arch/arm/include/arch/stack-frame.h
+++ arch/arm/include/arch/stack-frame.h
@@ -22,6 +22,7 @@ struct jit_stack_frame {
 	unsigned long args[0];
 } __attribute__((packed));
 
+unsigned long arg_offset(struct stack_frame *frame, unsigned long idx);
 unsigned long frame_local_offset(struct vm_method *, struct expression *);
 unsigned long slot_offset(struct stack_slot *slot);
 unsigned long slot_offset_64(struct stack_slot *slot);
