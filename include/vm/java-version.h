2ee893f4c5f182d5b0538c3c6f6ca0af2e6f7950
Pekka Enberg <penberg@kernel.org>
2012-02-17 16:12:38 +0200

    vm: Fix JVM defined system properties
    We advertise Java 1.6 version with "jato -version" but we also need to do that
    for JVM defined system properties. This fixes Jenkins early startup
    java.lang.UnsupportedClassVersionError exception.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- include/vm/java-version.h
+++ include/vm/java-version.h
@@ -4,6 +4,9 @@
 #include "vm/jni.h"
 
 #define JAVA_VERSION		"1.6.0"
+#define CLASSFILE_VERSION	"50.0"
+#define JAVA_SPEC_VERSION	"1.6"
+#define JAVA_VM_SPEC_VERSION	"1.0"
 #define JNI_VERSION		JNI_VERSION_1_6
 
 #endif
