4befb03605bf270a6f4eea0d870911a3522d03a3
Pekka Enberg <penberg@kernel.org>
2012-08-22 17:26:56 +0000

    jit/llvm: Add LLVM translation for OPC_DUP2_X1
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- jit/llvm/core.c
+++ jit/llvm/core.c
@@ -743,7 +743,37 @@ static int llvm_bc2ir_insn(struct llvm_context *ctx, unsigned char *code, unsign
 
 		break;
 	}
-	case OPC_DUP2_X1:		assert(0); break;
+	case OPC_DUP2_X1: {
+		void *value1, *value2;
+
+		value1 = stack_pop(ctx->mimic_stack);
+
+		value2 = stack_pop(ctx->mimic_stack);
+
+		assert(llvm_is_category_1_type(LLVMTypeOf(value2)));
+
+		if (llvm_is_category_1_type(LLVMTypeOf(value1))) {
+			void *value3;
+
+			value3 = stack_pop(ctx->mimic_stack);
+
+			assert(llvm_is_category_1_type(LLVMTypeOf(value3)));
+
+			stack_push(ctx->mimic_stack, value2);
+
+			stack_push(ctx->mimic_stack, value1);
+
+			stack_push(ctx->mimic_stack, value3);
+		} else {
+			stack_push(ctx->mimic_stack, value1);
+		}
+
+		stack_push(ctx->mimic_stack, value2);
+
+		stack_push(ctx->mimic_stack, value1);
+
+		break;
+	}
 	case OPC_DUP2_X2:		assert(0); break;
 	case OPC_SWAP: {
 		LLVMValueRef value1, value2;
