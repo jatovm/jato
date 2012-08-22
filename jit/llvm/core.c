a1d26428381b3bb4a057e6f7bda03e80db71bc25
Pekka Enberg <penberg@kernel.org>
2012-08-22 18:44:59 +0300

    jit/llvm: Add LLVM translation for OPC_FSTORE_n
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- jit/llvm/core.c
+++ jit/llvm/core.c
@@ -583,10 +583,23 @@ static int llvm_bc2ir_insn(struct llvm_context *ctx, unsigned char *code, unsign
 
 		break;
 	}
-	case OPC_FSTORE_0:		assert(0); break;
-	case OPC_FSTORE_1:		assert(0); break;
-	case OPC_FSTORE_2:		assert(0); break;
-	case OPC_FSTORE_3:		assert(0); break;
+	case OPC_FSTORE_0:
+	case OPC_FSTORE_1:
+	case OPC_FSTORE_2:
+	case OPC_FSTORE_3: {
+		LLVMValueRef value, addr;
+		uint16_t idx;
+
+		idx	= opc - OPC_FSTORE_0;
+
+		addr	= llvm_lookup_local(ctx, idx, LLVMFloatType());
+
+		value	= stack_pop(ctx->mimic_stack);
+
+		LLVMBuildStore(ctx->builder, value, addr);
+
+		break;
+	}
 	case OPC_DSTORE_0:		assert(0); break;
 	case OPC_DSTORE_1:		assert(0); break;
 	case OPC_DSTORE_2:		assert(0); break;
