9c9af25e7a0d4eed1eedb1db6d50164bd52e9893
Pekka Enberg <penberg@kernel.org>
2013-02-02 07:57:38 +0000

    x86: Fix up whitespace in comment
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- arch/x86/include/arch/instruction.h
+++ arch/x86/include/arch/instruction.h
@@ -102,7 +102,7 @@ enum insn_type {
 	INSN_AND_REG_REG,
 	INSN_CALL_REG,
 	INSN_CALL_REL,
-	INSN_CLTD_REG_REG,	/* CDQ in Intel manuals*/
+	INSN_CLTD_REG_REG,	/* CDQ in Intel manuals */
 	INSN_CMP_IMM_REG,
 	INSN_CMP_MEMBASE_REG,
 	INSN_CMP_REG_REG,
